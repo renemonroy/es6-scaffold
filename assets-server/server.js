9f49245e031e2006049adf036f7f27b17b13c58a
René Monroy <rene.monroy@gmail.com>
2015-05-23 23:05:32 -0500

    Create webpack server

---
+++ assets-server/server.js
@@ -0,0 +1,21 @@
+var config = require('./config');
+process.env.NODE_ENV = config.env;
+
+var webpack = require('webpack'),
+  WebpackDevServer = require('webpack-dev-server'),
+  webpackConfig = require('./webpack.config.js');
+
+var bundleConfig = {
+  contentBase : __dirname,
+  hot : false,
+  quiet : false,
+  noInfo : false,
+  inline : true,
+  publicPath : webpackConfig.output.publicPath,
+  stats : { colors : true },
+  historyApiFallback : true,
+  headers: { 'Access-Control-Allow-Origin': '*' }
+};
+
+var server = new WebpackDevServer(webpack(webpackConfig), bundleConfig);
+server.listen(config.port, config.ip, function() {});
