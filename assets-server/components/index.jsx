b1b87af469f2495571400d1467cc5770952ef334
René Monroy <rene.monroy@gmail.com>
2015-06-03 23:18:50 -0500

    Attach callback inside the app class loader

--- assets-server/components/index.jsx
+++ assets-server/components/index.jsx
@@ -3,8 +3,6 @@
 import React from 'react/addons';
 import App from './app.jsx';
 
-const loadApp = () => {
-  React.renderComponent(<App />, document.body);
-};
-
-document.addEventListener('DOMContentLoaded', loadApp);
+document.addEventListener('DOMContentLoaded', e => {
+  React.render(<App />, document.body);
+});
