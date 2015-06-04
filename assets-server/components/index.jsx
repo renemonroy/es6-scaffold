b95113dc6a4b50c538e1125810dd857054489d03
René Monroy <rene.monroy@gmail.com>
2015-06-03 22:27:25 -0500

    Change index dependencies paths

--- assets-server/components/index.jsx
+++ assets-server/components/index.jsx
@@ -1,12 +1,8 @@
-(function(win, doc) {
+import React from 'react/addons';
+import App from './app.jsx';
 
-  import React from 'react/addons';
-  import App from './app/app.jsx';
+const loadApp = () => {
+  React.renderComponent(<App />, document.body);
+};
 
-  const loadApp = () => {
-    React.renderComponent(<App />, document.body);
-  };
-
-  document.addEventListener('DOMContentLoaded', loadApp);
-
-})(window, document);
+document.addEventListener('DOMContentLoaded', loadApp);
